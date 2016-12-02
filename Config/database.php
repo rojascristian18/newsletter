892dbe3eaf281a7340d61e74fa4f8517934d949b
Cristian Rojas Pérez <Cristian Rojas Prez>
2016-12-02 16:59:19 -0300

    Configuración del Sistema

---
+++ Config/database.php
@@ -0,0 +1,26 @@
+<?php
+class DATABASE_CONFIG
+{
+	public $default = array(
+		'datasource'	=> 'Database/Mysql',
+		'persistent'	=> false,
+		'host'			=> 'localhost',
+		'login'			=> 'root',
+		'password'		=> '',
+		'database'		=> 'newsletter',
+		'prefix'		=> 'news_',
+		'encoding'		=> 'utf8'
+	);
+
+	/* Data source toolmania */
+	public $toolmania = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '69.164.205.133',
+		'login' => 'nodriza',
+		'password' => 'IgP_8111980_IgP',
+		'database' => 'toolmania',
+		'prefix' => 'tm_',
+		'encoding' => 'utf8',
+	);
+}
